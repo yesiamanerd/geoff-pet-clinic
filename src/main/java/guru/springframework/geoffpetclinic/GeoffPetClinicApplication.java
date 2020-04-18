1adf6b073a448f82d32176a295edb631bb7110f5
yesiamanerd <hepsubah>
2020-04-17 20:45:03 -0400

    Initial Project Add

---
+++ src/main/java/guru/springframework/geoffpetclinic/GeoffPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.geoffpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoffPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoffPetClinicApplication.class, args);
+    }
+
+}
