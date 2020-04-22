18b24904e3574853dd7b2672e4ec4560ccb01336
yesiamanerd <hepsubah>
2020-04-22 12:23:57 -0400

    Added Crud Service common interface.  closes #11

--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/OwnerService.java
@@ -2,13 +2,6 @@ package guru.springframework.geoffpetclinic.services;
 
 import guru.springframework.geoffpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String name);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 }
--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package guru.springframework.geoffpetclinic.services;
 
 import guru.springframework.geoffpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
