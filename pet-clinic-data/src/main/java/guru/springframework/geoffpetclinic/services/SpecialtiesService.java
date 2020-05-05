83f628e5d9c89a12fa3d15aa1a9f8fa88cdc8a70
yesiamanerd <hepsubah>
2020-05-05 16:27:07 -0400

    Created Specialty Map Service Impl .  closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.geoffpetclinic.services;
+
+import guru.springframework.geoffpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.geoffpetclinic.services.map;
+
+import guru.springframework.geoffpetclinic.model.Speciality;
+import guru.springframework.geoffpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap  extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
