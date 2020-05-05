32f6374950a3ec45137f494b5d2c962f4d5a3dd3
yesiamanerd <hepsubah>
2020-05-05 14:52:38 -0400

    Created Pet Type Map Service Impl.  closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.geoffpetclinic.services;
+
+import guru.springframework.geoffpetclinic.model.PetType;
+
+public interface PetTypeService  extends CrudService<PetType, Long>{
+}
