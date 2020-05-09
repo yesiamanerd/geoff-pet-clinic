9d8668a2796eefee1d5df139f179ddad39b462a5
yesiamanerd <hepsubah>
2020-05-09 15:47:48 -0400

    Updated visits with JPA annotations.  closes #38

--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Vet.java
@@ -11,7 +11,7 @@ public class Vet extends Person {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties",
             joinColumns = @JoinColumn(name = "vet_id"),
-            inverseJoinColumns = @JoinColumn("speciality_id"))
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
