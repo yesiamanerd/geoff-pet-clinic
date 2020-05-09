c1faaddfb52fc24d03e4cba4d96e04888fb943b0
yesiamanerd <hepsubah>
2020-05-09 12:20:25 -0400

    Updated owners with JPA annotations.  closes #36

--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Person.java
@@ -1,8 +1,17 @@
 package guru.springframework.geoffpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
+//    @NotEmpty
     private String firstName;
+
+    @Column(name = "last_name")
+//    @NotEmpty
     private String lastName;
 
     public String getFirstName() {
