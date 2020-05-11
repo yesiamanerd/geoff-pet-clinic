eda284d5ce320f0e1a4997fbeb67cd1e34c89926
yesiamanerd <hepsubah>
2020-05-10 20:53:59 -0400

    Implemented Map Impl for Visit Service.  closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package guru.springframework.geoffpetclinic.services;
+
+import guru.springframework.geoffpetclinic.model.Visit;
+
+public interface VisitService  extends CrudService<Visit, Long> {
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/services/map/SpecialtyMapService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class SpecialtyServiceMap  extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
     @Override
     public Set<Speciality> findAll() {
         return super.findAll();
