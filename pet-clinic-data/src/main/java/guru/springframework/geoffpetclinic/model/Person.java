7278c4b2365c574a8602e6b9e97b2c4c19832430
yesiamanerd <hepsubah>
2020-05-18 15:38:16 -0400

    added Project Lombok.  closes #51

--- pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/geoffpetclinic/model/Person.java
@@ -1,32 +1,29 @@
 package guru.springframework.geoffpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
 
     @Column(name = "first_name")
-//    @NotEmpty
     private String firstName;
 
     @Column(name = "last_name")
-//    @NotEmpty
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
